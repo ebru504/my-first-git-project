937f32a6e145739558adb4fc13374830fc42f5df
Ebru Eroglu <ebrueroglu@Ebrus-MacBook-Air.local>
2022-03-28 01:49:44 +0100

    test

--- src/firstFile/firstClass.java
+++ src/firstFile/firstClass.java
@@ -3,11 +3,14 @@ package firstFile;
 public class firstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        divide(4,2);
+
     }
 
     public static int divide(int a, int b){
         return a/b;
     }
+
 
 
 
