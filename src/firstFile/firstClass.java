f1dc745798f693537e84a41aa752b0661d7ad85b
Ebru Eroglu <ebrueroglu@Ebrus-MacBook-Air.local>
2022-03-28 01:42:23 +0100

    divide method added

--- src/firstFile/firstClass.java
+++ src/firstFile/firstClass.java
@@ -4,4 +4,11 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static int divide(int a, int b){
+        return a/b;
+    }
+
+
+
 }
