3dbe3967b820bb7dd47556d5020529405ce5477c
Ebru Eroglu <ebrueroglu@Ebrus-MacBook-Air.local>
2022-04-04 12:57:07 +0100

    second class

---
+++ src/firstFile/secondClass.java
@@ -0,0 +1,16 @@
+package firstFile;
+
+import java.util.Scanner;
+
+public class secondClass {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter int");
+        int i = scan.nextInt();
+        System.out.println("yor int is " + i);
+
+
+
+    }
+}
